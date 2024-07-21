ccfdddfae63ad587957b603b4a70f0902fdc9476
Emiliomv14 <angelemiliomoralesvargas08@gmail.com>
2024-07-20 22:46:47 -0600

    Correcciones y Blur

--- Desintox/Assets/Scripts/GameControllers/GCInicio02.cs
+++ Desintox/Assets/Scripts/GameControllers/GCInicio02.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -26,20 +27,19 @@ public class GCInicio02 : MonoBehaviour
         texto_volumen.text = slider_volumen.value.ToString(); //Actualiza el valor en el cuadrado sobre el slider del sonido para ver el volumen
     }
 
-    public void Boton_Iniciar_Partida()//Se activa al presionar el boton 'Iniciar Partida ' del menu principal
+    public void Boton_Iniciar_Partida()
     {
-        StartCoroutine(CargarEscena());//Inicia la corrutina encargada de cambiar de escena y presentar posibles efectos de transición
+        StartCoroutine(CargarEscena());
     }
 
     public void Boton_Opciones()//Se activa al presionar el boton 'opciones' del menu principal
     {
-        canvas_principal.SetActive(false);//Apaga el canvas principal
-        canvas_opciones.SetActive(true);//Enciende el canvas que contiene las opciones
+        canvas_opciones.SetActive(true);
+        
     }
     public void Boton_Regresar()//Se activa al presionar el boton 'regresar' en el menu de opciones
     {
-        canvas_principal.SetActive(true);//Apaga el canvas de las opciones
-        canvas_opciones.SetActive(false);//Enciende el canvas principal
+        canvas_opciones.SetActive(false);
     }
     public void Boton_Salir()//Se activa al presionar el boton 'Salir' del menu principal
     {
@@ -50,8 +50,8 @@ public class GCInicio02 : MonoBehaviour
     private IEnumerator CargarEscena()//Corrutina de cambio de escena y carga de efectos
     {
         //Aquí podemos agregar algún tipo de efecto como fundido o algo que se vea más chido.
-        yield return new WaitForSeconds(1f); //Este esperará para que se pueda ver el efecto antes de cambiar la escena.
-        SceneManager.LoadSceneAsync("NuevoJuego"); //Cambiamos de escena (parece que no existen cambios en este apartado a la hora de programar para android).
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadSceneAsync("NuevoJuego");
         //Usamos LoadSceneAsyn para que la escena cargue en segundo plano en lo que el efecto se haga.
     }
 }
