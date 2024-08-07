c03abee4fb67962c87a3fc44cfb3209b838f2e78
MyCodeIsDumb <136141313+MyCodeIsDumb@users.noreply.github.com>
2024-08-07 16:28:22 -0600

    preguntas

--- Desintox/Assets/Scripts/GameControllers/GCJuego.cs
+++ Desintox/Assets/Scripts/GameControllers/GCJuego.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class GCJuego : MonoBehaviour
 {
     //RandomsUnity -->  https://rusbenguzman.medium.com/generating-random-numbers-in-unity-spanish-ddd63e7795e
-    private int[] uniqueNumbers = new int[6];
+    private int[] uniqueNumbers = new int[20];
     public Canvas preguntas;
-    private int Counter = -1;
+    private int Counter = 0;
 
     void Start()
     {
@@ -16,7 +16,7 @@ public class GCJuego : MonoBehaviour
 
     int[] GenerateUniqueNumbers()
     {
-        int[] numbers = { 0, 1, 2, 3, 4, 5};
+        int[] numbers = { 0, 1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
         // Mezclar el arreglo
         for (int i = 0; i < numbers.Length; i++)
         {
@@ -29,7 +29,7 @@ public class GCJuego : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.V)&& Counter<(uniqueNumbers.Length-1))
+        if (Input.GetKeyDown(KeyCode.V)&& Counter<(uniqueNumbers.Length))
         {
             bool isActive = !preguntas.gameObject.activeSelf;
             if (preguntas != null)
--- Desintox/Assets/Scripts/Pantallas emergentes/preguntas.cs
+++ Desintox/Assets/Scripts/Pantallas emergentes/preguntas.cs
@@ -11,24 +11,75 @@ public class preguntas : MonoBehaviour
     public Button True, False;
     private int[] receivedNumbers;
     public TextMeshProUGUI Question_txt;
-    private bool isQuestionAnswered;
-    int Counter=0;
     public Canvas Preguntas;
     public string[] Questionario = new string[]{
-        "Test_true",
-        "Test_false",
-        "Test_true",
-        "Test_false",
-        "Test_true",
+    "¿Las drogas en México se declararon ilegales en el año 1940?",
+    "El término “adicciones” se refiere solo al abuso y consumo de sustancias tales como el tabaco, la bebida y las drogas",
+    "¿México cuenta con Centro de Atención Ciudadana contra las Adicciones?",
+    "Realizar de forma constante y excesiva actividades como: el ejercicio, los videojuegos y redes sociales, ¿cuenta como adicciones?",
+    "¿Jugar constantemente juegos de azar en casinos se puede considerar una adicción?",
+    "¿Jugar a videojuegos, juegos de azar y pasar el rato en redes sociales  son actividades que directamente vuelven adictos a las personas?",
+    "¿El ácido sulfúrico puede utilizarse para la elaboración de drogas como la cocaína?",
+    "¿El ácido acetil salicílico es el nombre científico de la marihuana?",
+    "No pasa nada si sólo se consumen drogas los fines de semana",
+    "¿Las personas con dependencia a las drogas tomaron una decisión equivocada?",
+    "¿SI TOMO MUCHO ALCOHOL ME HARÉ MENOS RESISTENTE?",
+    "¿PARA PASARLO BIEN HAY QUE BEBER ALCOHOL?",
+    "¿UNA INTOXICACIÓN GRAVE CON ALCOHOL PUEDE PRODUCIR LA MUERTE?",
+    "¿EL ALCOHOL BAJA LA TEMPERATURA CORPORAL?",
+    "¿EL CONSUMO DE MARIHUANA NO HACE DAÑO?",
+    "¿EL ALCOHOL MATA NEURONAS?",
+    "¿EL CONSUMO EN EDAD TEMPRANO TIENE CONSECUENCIAS?",
+    "MIENTRAS MÁS JOVEN SE COMIENCE A CONSUMIR DROGAS, MÁS PROBABILIDADES HAY DE SER DEPENDIENTE",
+    "¿EL ALCOHOL NOS PONE “BIEN HAPPY 'S”?",
+    "LA MARIHUANA NO AFECTA LOS PULMONES"
+    };
+    public string[] Facts = new string[]{
+    ": En 1940, último año de su mandato como presidente, Lázaro Cárdenas promulgó un Reglamento Federal de Toxicomanías en el que se eliminaron diversos decretos punitivos que consideraban el consumo, posesión y venta de drogas como un delito.",
+    ": La adicción es una enfermedad neurológica que se caracteriza por una búsqueda patológica de la recompensa o alivio a través del uso de una sustancia u otras acciones.",
+    ": En el Centro de Atención Ciudadana contra las Adicciones un grupo de especialistas te escucha para ofrecerte: Prevención y atención del consumo de sustancias adictivas y orientación a sus familiares. Tratamiento breve para dejar de fumar. Entre otros servicios.",
+    ": El riesgo de caer en una adicción es parte de la vida. Lo que sucede es que a veces no lo percibimos. Los seres humanos podemos crear dependencia hacia cualquier cosa, incluso hacia el fútbol, señala el doctor Hugo España, toxicólogo y especialista en adicciones.",
+    ": La ludopatía es un trastorno de orden psicológico que lleva a la persona a jugar y apostar repetidamente en un casino u otro sistema de apuestas, afectando de forma negativa a la vida personal, familiar y vocacional. También se aplica a la adicción patológica a los juegos electrónicos en general.",
+    ": Se ha demostrado que estas actividades por sí mismas no generan dependencia, ludopatía o adicción y si bien se registran casos que ligan a estas actividades la realidad es que casi cualquier actividad sin medida puede generar adicción en una persona.",
+    ": El ácido sulfúrico, acetona, permanganato de potasio, etc. Es extraído hasta su forma final en cocaína (clorhidrato de cocaína).",
+    ": El ácido acetilsalicílico o AAS, conocido popularmente como aspirina, nombre de una marca que pasó al uso común, es un fármaco de la familia de los salicilatos. Se utiliza como medicamento para tratar el dolor, la fiebre y la inflamación, debido a su efecto inhibidor, no selectivo, de la ciclooxigenasa.",
+    ": Debe recordarse que algunos efectos graves del consumo como es el caso del “golpe de calor” son independientes del tiempo que se lleve consumiendo. Por otra parte, consumir todos los fines de semana conlleva un riesgo evidente. Además hay que tener en cuenta que los efectos del fin de semana se prolongan durante varios días.",
+    ": Desde el 2019, los Estados Miembro de las Naciones Unidas han reconocido que la dependencia a las drogas es un asunto de salud pública relacionado con múltiples factores. Esto significa que existen diversas variables que incrementan la posibilidad de que alguien comience a utilizar drogas o desarrolle problemas de salud mental por su consumo.",
+    ": El consumir frecuentemente el alcohol te generará una resistencia cada vez más fuerte al punto donde no será suficiente con unas pocas copas.",
+    ": El alcohol produce una “buena onda” temporal, que termina cuando se acaba el efecto. Beber en exceso puede traer un buen momento, pero luego podemos sentirnos cansados, deprimidos o nerviosos. Unas copas de más a veces puede traernos una “buena resaca”.",
+    ": Si la intoxicación es grave, existe un alto riesgo de morir, ya que puede causar parálisis respiratoria y compromiso cardiovascular. También puede provocar pérdida de conocimiento, problemas respiratorios, gastritis crónica y alterar el funcionamiento del hígado, lo que puede llevar finalmente a una cirrosis hepática. Además puede provocar una hepatitis aguda que eventualmente puede causar la muerte.",
+    ": Al llegar al cerebro el alcohol activa unos centros que causan caída en la temperatura corporal; por eso, la hipotermia es una de las consecuencias más rápidas y contundentes del consumo de alcohol.",
+    ": La marihuana puede generar dependencia. Su consumo produce dificultades en la capacidad de aprendizaje, problemas de concentración, altera la memoria inmediata y desmotiva.",
+    ": Sí se ha comprobado que el alcohol causa degeneración neuronal y dificulta la comunicación entre las neuronas.",
+    ": El consumo temprano tiene consecuencias graves en niños, niñas y adolescentes, y se debe evitar en todas las circunstancias.",
+    ": Diversas investigaciones señalan que mientras más temprano se inicie el consumo de drogas, más probabilidades hay de generar una adicción (dependencia de las drogas). Esto se debe a que el organismo joven se encuentra en desarrollo y, por lo tanto, es más vulnerable a los efectos de las drogas.",
+    ": El alcohol hace que la gente se desinhiba, pero hay quienes al beber tienen reacciones negativas que pueden provocar problemas con la ley, violencia, conflictos con la familia, la pareja o en el trabajo.",
+    ": La marihuana afecta los pulmones porque el humo se retiene en ellos, conteniendo monóxido de carbono. Esto inflama los pulmones y produce riesgos de contraer enfermedades como la neumonía, bronquitis crónica, etc."
     };
     public bool[] Results = new bool[]{
-        true,
-        false,
-        true,
-        false,
-        true,
+    true,
+    false,
+    true,
+    true,
+    true,
+    false,
+    true,
+    false,
+    false,
+    false,
+    false,
+    true,
+    true,
+    true,
+    false,
+    true,
+    true,
+    true,
+    false,
+    false
     };
 
+    int Counter=0;
     void Start()
     {
         // Asignar eventos a los botones
@@ -59,20 +110,13 @@ public class preguntas : MonoBehaviour
         if (receivedNumbers != null)
         {
                 Question_txt.text = Questionario[receivedNumbers[Counter]];
-                //Debug.Log($"Index: {receivedNumbers[Counter]}, Pregunta actual: {Questionario[receivedNumbers[Counter]]}, Respuesta correcta: {Results[receivedNumbers[Counter]]}");
 
         }
-        else
-        {
-            //Debug.LogError("receivedNumbers o Counter no están inicializados correctamente.");
-        }
-
     }
 
     void AnswerQuestion(bool answer)
     {
         bool correctAnswer = Results[receivedNumbers[Counter]];
-        //Debug.Log($"Index: {receivedNumbers[Counter]}, Answer: {answer}, Correct Answer: {correctAnswer}");
         // Validar la respuesta
         if (answer == correctAnswer)
         {
