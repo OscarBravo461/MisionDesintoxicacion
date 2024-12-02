5c5ce5f8d54d4c22ad45760139b440ba6a5889e0
Titomito3 <sebastiansevero27@gmail.com>
2024-12-02 15:23:00 -0600

    YA SUENA LA PERRA MAMADA ESTAMOS SE LOGRO
    AHUEVOOOOOO

---
+++ Desintox/Assets/Scripts/Audio/Prueba/MusicArea.cs
@@ -0,0 +1,10 @@
+public enum MusicArea
+{
+    Selec_Perso = 0,
+    Plaza = 1,
+    Parque = 2,
+    Panta_ini = 3,
+    Escuela = 4,
+    Edificios_castigo = 5,
+    Ajustes = 6
+}
---
+++ Desintox/Assets/Scripts/Audio/Prueba/VolumeSlider.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    private enum VolumeType {
+        MASTER,
+        MUSIC,
+        AMBIENCE,
+        SFX
+    }
+
+    [Header("Type")]
+    [SerializeField] private VolumeType volumeType;
+
+    private Slider volumeSlider;
+
+    private void Awake()
+    {
+        volumeSlider = this.GetComponentInChildren<Slider>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void OnSliderValueChanged()
+    {
+        
+    }
+}
